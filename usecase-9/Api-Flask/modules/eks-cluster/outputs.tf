93575e418a759bcc52c954ce9574dfbc5f441067
narendiransankar <narendiransankar1699@gmail.com>
2025-07-25 12:24:15 +0530

    Delete usecase-9/Api-Flask/modules directory

--- usecase-9/Api-Flask/modules/eks-cluster/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "cluster_name" {
-  description = "The name of the EKS cluster"
-  value       = aws_eks_cluster.this.name
-}
--- usecase-9/Api-Flask/modules/eks-cluster/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "cluster_name" {}
-variable "cluster_role_arn" {}
-variable "subnet_ids" {
-  type = list(string)
-}
